82f96601a6df6530e865283dffdc5f411ff78be7
Danilo Moraes <danilolopesdemoraes@gmail.com>
2021-03-10 09:07:07 -0300

    Complete all requirements

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!!'});
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Upgrade to PRO plan.' });
+  }
+
+  if (user.pro) {
+    return next();
+  }
+
+  return next();
+  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Invalid project ID' });
+}
+
+  const user = users.find((user) => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!!'});
+  }
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if (todoIndex < 0) {
+    return response.status(404).json({error: 'TODO not found!!'});
+  }
+
+  request.user = user;
+  request.todo = user.todos[todoIndex];
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+  
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!!'});
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
